86d753a4c115893a1db1f3f37c55bcbb5f413422
Alex-Diez <alexvic.amatory@gmail.com>
2016-11-18 09:55:36 +0200

    ip mask kata day eighth

---
+++ ip-mask-kata/day-8.py
@@ -0,0 +1,31 @@
+# -*- codeing: utf-8 -*-
+
+import math
+
+class Resolver(object):
+
+    def resolve(self, ip_mask):
+        return sum(self._value_of(octet) for octet in ip_mask.split("."))
+
+    def _value_of(self, octet):
+        return 8 - math.log2(256 - int(octet))
+
+import unittest
+
+
+class MaskResolverTest(unittest.TestCase):
+
+    def setUp(self):
+        self.resolver = Resolver()
+
+    def test_resolves_four_octets(self):
+        self.assertEqual(32, self.resolver.resolve("255.255.255.255"))
+
+    def test_resolves_three_octets(self):
+        self.assertEqual(24, self.resolver.resolve("255.255.255.0"))
+
+    def test_resolves_two_octets(self):
+        self.assertEqual(16, self.resolver.resolve("255.255.0.0"))
+
+    def test_resolves_255_255_254_0(self):
+        self.assertEqual(23, self.resolver.resolve("255.255.254.0"))
