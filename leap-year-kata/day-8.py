2483723aac7d011686a71f9c31c9f1faa21657b0
Alex-Diez <alexvic.amatory@gmail.com>
2016-12-28 17:50:53 +0200

    leap year kata eighth day

---
+++ leap-year-kata/day-8.py
@@ -0,0 +1,20 @@
+# -*- codeing: utf-8 -*-
+
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+import unittest
+
+class LeapYearTest(unittest.TestCase):
+
+    def test_leap_year_is_divided_by_4(self):
+        self.assertTrue(is_leap_year(1996))
+
+    def test_simple_year_is_not_divided_by_4(self):
+        self.assertFalse(is_leap_year(1995))
+
+    def test_simple_year_is_divided_by_100(self):
+        self.assertFalse(is_leap_year(1900))
+
+    def test_leap_year_is_divided_by_400(self):
+        self.assertTrue(is_leap_year(2000))
